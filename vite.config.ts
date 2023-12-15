25a6fee46bd904442d22218c1c8f13d4ac2951b7
Rebecka Göthlin <146741778+RebeckaGothlin@users.noreply.github.com>
2023-12-15 12:50:14 +0100

    Update vite.config.ts
    Change to our name

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  'base': '/fed23d-js-grupparbete-mall/' // TODO: Ändra till ert reponamn här
+  'base': '/fed23d-javascript-grundkurs-gruppuppgift-bergamotterna/' // TODO: Ändra till ert reponamn här 
 });
